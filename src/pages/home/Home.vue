ad83724a779c72f99cd519f4f36aaf2a43d6b40f
ZiJie.Yip <438623841@qq.com>
2018-12-13 00:26:41 +0800

    添加Home页面

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="home">
+            Home
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+    .home{
+        font-size: 50px;
+    }
+</style>
