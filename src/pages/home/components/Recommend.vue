8713df8c83cf845dac52264cc0fd1f97ab969951
ZiJie.Yip <438623841@qq.com>
2018-12-18 23:49:01 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.img"/>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        img: 'http://img1.qunarzz.com/sight/p0/1611/96/96c7b547622c8334a3.img.jpg_200x200_cd0b30b0.jpg',
+        title: '西山温泉',
+        desc: '浪漫的西山温泉'
+      }, {
+        id: '0001',
+        img: 'http://img1.qunarzz.com/sight/p0/1611/96/96c7b547622c8334a3.img.jpg_200x200_cd0b30b0.jpg',
+        title: '西山温泉',
+        desc: '浪漫的西山温泉'
+      }, {
+        id: '0001',
+        img: 'http://img1.qunarzz.com/sight/p0/1611/96/96c7b547622c8334a3.img.jpg_200x200_cd0b30b0.jpg',
+        title: '西山温泉',
+        desc: '浪漫的西山温泉'
+      }, {
+        id: '0001',
+        img: 'http://img1.qunarzz.com/sight/p0/1611/96/96c7b547622c8334a3.img.jpg_200x200_cd0b30b0.jpg',
+        title: '西山温泉',
+        desc: '浪漫的西山温泉'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/mixins'
+  .recommend-title
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+    margin-top: .2rem
+  .item
+    overflow hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        margin-top: .16rem
+        color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="recommend-title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.img"/>
+        </div>
+
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        img: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+        title: '西山温泉',
+        desc: '浪漫的西山温泉'
+      }, {
+        id: '0001',
+        img: 'http://img1.qunarzz.com/sight/source/1602/9c/39c5ce9ff58609.jpg_r_640x214_efb90102.jpg',
+        title: '西山温泉',
+        desc: '浪漫的西山温泉'
+      }, {
+        id: '0001',
+        img: 'http://img1.qunarzz.com/sight/source/1811/f8/29dfa785277839.jpg_r_640x214_7d051523.jpg',
+        title: '西山温泉',
+        desc: '浪漫的西山温泉'
+      }, {
+        id: '0001',
+        img: 'http://img1.qunarzz.com/sight/p0/1611/96/96c7b547622c8334a3.img.jpg_200x200_cd0b30b0.jpg',
+        title: '西山温泉',
+        desc: '浪漫的西山温泉'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/mixins'
+  .recommend-title
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+    margin-top: .2rem
+  .item-img-wrapper
+    overflow hidden
+    height: 0
+    padding-bottom: 33.9%
+  .item-img
+    width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
