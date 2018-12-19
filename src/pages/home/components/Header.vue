29666c9a85c9bcce8be4cad230b34d5fd431832b
ZiJie.Yip <438623841@qq.com>
2018-12-20 00:24:34 +0800

    city-router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -28,7 +30,7 @@ export default {
   .header
     background: $bgColor
     display: flex
-    line-height: .86rem
+    line-height: $headerHeight
     color: #fff
     .header-left
       width: .64rem
@@ -40,6 +42,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      color: #fff
       .arrow-icon
         margin-left: -.04rem
         font-size: .24rem
